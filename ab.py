99dcc5e72c0ec777c0d3efd216be06b34a76f86f
AntonyBarahona <barahouf25@gmail.com>
2023-02-18 12:21:54 -0600

    Calculo de cuanto tarda un carro

---
+++ ab.py
@@ -0,0 +1,4 @@
+distancia=int(input("Coloque la distancia que recorrerà "))
+velocidad=int(input("coloque la velocidad a la que va "))
+tiempo=distancia/velocidad
+print("llego en "+str(tiempo)+"horas")
